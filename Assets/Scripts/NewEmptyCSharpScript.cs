fccd6eb12f1db3e9d3e5a5ca4994eaa653492d8f
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-11 19:49:16 +0700

    Добавьте файлы проекта.

---
+++ Assets/Scripts/NewEmptyCSharpScript.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEmptyCSharpScript
+{
+  int[] numbersArrray = new int[] { 1, 2, 3 };
+  List<int> evenNumbersArray = new List<int>() { 2, 4, 6 };
+  Dictionary<int, string> anotherNumbers = new Dictionary<int, string>()
+  {
+    {1, "one"},
+    {2, "two"},
+    {3, "three"},
+    {4, "four"}
+  };
+  int someIntValue = 5;
+  float someFloatValue = 3.14f;
+
+ }
